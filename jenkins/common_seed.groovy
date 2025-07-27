7484a29251cee0df06308fc6e5fa5f229d2536eb
sameer27patil <71928005+sameer27patil@users.noreply.github.com>
2025-07-27 18:55:10 +0530

    Update common_seed.groovy
    git url updated

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,4 +1,4 @@
-def baseUrl = "https://github.com/ybmadhu/"
+def baseUrl = "https://github.com/jmstechhome18/spring3-mvc-maven-xml-hello-world.git"
 def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
