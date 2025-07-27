226b80819ea8991885cf302c227b1a71c61a621a
sameer27patil <71928005+sameer27patil@users.noreply.github.com>
2025-07-27 23:42:39 +0530

    Create pushMavenEcrRepo.groovy

---
+++ vars/pushMavenEcrRepo.groovy
@@ -0,0 +1,52 @@
+def call(Map pipelineParams){
+def projectName = "${pipelineParams.ecrRepoName}"
+
+pipeline {
+   agent any
+   environment
+    {
+        VERSION = "v${BUILD_NUMBER}"
+        PROJECT = "${projectName}"
+        IMAGE = "$PROJECT:$VERSION"
+        ECRURL = 'https://890742609766.dkr.ecr.ap-south-1.amazonaws.com/${projectName}'
+        ECRCRED = 'ecr:ap-south-1:aws_credentials'
+    }
+    stages {
+      stage('build'){
+             
+             steps{
+                 sh 'mvn package'
+             }
+         }
+         stage('Image Build'){
+             steps{
+                 script{
+                       docker.build(IMAGE)
+                 }
+             }
+         }
+         stage('Push Image'){
+         steps{
+             script
+                {
+
+                    docker.withRegistry(ECRURL, ECRCRED)
+                    {
+                        docker.image(IMAGE).push()
+                    }
+                }
+            }
+         }
+    }
+
+    post
+    {
+        always
+        {
+            // make sure that the Docker image is removed
+            sh "docker rmi $IMAGE | true"
+        }
+    }
+
+}
+}
